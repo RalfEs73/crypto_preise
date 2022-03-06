7faa41f67d14a19db4a576af4f228ad2b39d7c09
RalfEs73 <ralf@essbaumer.de>
2022-03-06 23:22:09 +0100

    New features

---
+++ btc-rechner.ps1
@@ -0,0 +1,15 @@
+﻿$headers = @{
+    'X-CMC_PRO_API_KEY'='02372bda-5f7d-444b-891b-fee24172e440'
+    Content='application/json'
+}
+
+# BTC = 1
+# PNY = 3481
+
+$response = Invoke-RestMethod -Method Get -Uri 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion?id=1&amount=1&convert=USD' -Headers $headers
+$data = $response | ConvertTo-Json
+$response.data[0].name
+$response.data[0].quote[0].USD[0].price
+$Number = Read-Host "Anzahl der"$response.data[0].name
+$temp = $response.data[0].quote[0].USD[0].price * $Number
+$temp
