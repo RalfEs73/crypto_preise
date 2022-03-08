b94dcf6eb16e833c2ad5912bf07850c7300ad9ed
RalfEs73 <ralf@essbaumer.de>
2022-03-08 23:18:29 +0100

    Create crypto-id.ps1

---
+++ crypto-id.ps1
@@ -0,0 +1,18 @@
+﻿$headers = @{
+    'X-CMC_PRO_API_KEY'='02372bda-5f7d-444b-891b-fee24172e440'
+    Content='application/json'
+}
+
+$temp = Read-Host "Welche Crypto"
+$response = Invoke-RestMethod -Method Get -Uri "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?symbol=$temp" -Headers $headers
+$data = $response | ConvertTo-Json
+
+$max = $response.data.length
+
+
+for ($i=0; $i -lt $max; $i++)
+	{
+	$response.data[$i].name
+	$response.data[$i].id
+    
+	}
