50c43b9fb57701a5ecd99003f997316f0d96ca6f
RalfEs73 <ralf@essbaumer.de>
2022-03-12 12:39:28 +0100

    Add some new Coins

---
+++ xmr.ps1
@@ -0,0 +1,22 @@
+﻿param (
+	[string]$crypto=""
+	)
+	
+$headers = @{
+    'X-CMC_PRO_API_KEY'='02372bda-5f7d-444b-891b-fee24172e440'
+    Content='application/json'
+}
+
+$crypto_id = 328
+$response = Invoke-RestMethod -Method Get -Uri "https://pro-api.coinmarketcap.com/v1/tools/price-conversion?id=$crypto_id&amount=1&convert=USD" -Headers $headers
+$data = $response | ConvertTo-Json
+# $response.data[0].name
+# $response.data[0].quote[0].USD[0].price
+
+Write-Host -ForegroundColor Red $response.data[0].name
+Write-Host "$"$response.data[0].quote[0].USD[0].price
+
+if($crypto) {
+	$temp = $response.data[0].quote[0].USD[0].price * $crypto
+	Write-Host "$"$temp
+}
