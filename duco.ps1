2ce34204eaf7796a1090b5582f8b4c3ae98eee45
RalfEs73 <ralf@essbaumer.de>
2023-03-06 23:58:09 +0100

    Create duco.ps1

---
+++ duco.ps1
@@ -0,0 +1,18 @@
+﻿param (
+	[string]$crypto=""
+	)
+	
+
+$headers = @{
+    'X-CMC_PRO_API_KEY'="$api"
+    Content='application/json'
+}
+
+
+Write-Host -ForegroundColor Red Duco
+Write-Host 417 Ducos sind ca. ein US Cent.
+
+if($crypto) {
+	$temp = (0.01/417) * $crypto
+	Write-Host "$"$temp
+}
